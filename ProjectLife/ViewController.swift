5b054a458f4d7f0f44cb521c93cad799a0adc6ef
liumengda <liumengda@bytedance.com>
2019-07-02 15:05:58 +0800

    intial commit

---
+++ ProjectLife/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProjectLife
+//
+//  Created by liumengda on 2019/7/2.
+//  Copyright © 2019 Unprecedented. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
