7203f3fa6cfc0da0c1df7505284a736134e2d140
刘梦达 <mengdaliu@berkeley.edu>
2019-07-18 13:03:17 +0800

    change font size

--- ProjectLife/ViewControllers/swiftFiles/SignIn.swift
+++ ProjectLife/ViewControllers/swiftFiles/SignIn.swift
@@ -25,19 +25,24 @@ class SignIn: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
-        line1.font = .labelFont(ofSize: CGFloat(60))
-        line2.font = .labelFont(ofSize: CGFloat(60))
-        buttonOutlet.font = .labelFont(ofSize: CGFloat(60))
+        line1.font = .labelFont(ofSize: CGFloat(50))
+        line2.font = .labelFont(ofSize: CGFloat(50))
+        buttonOutlet.font = .labelFont(ofSize: CGFloat(50))
         let loadedData = UserDefaults.standard.data(forKey: "Theme Color")
         var loadedColor : NSColor?
-        do {
-            try loadedColor =  NSKeyedUnarchiver.unarchivedObject(ofClasses : [NSColor.self], from: loadedData!) as? NSColor
-            if loadedColor != ThemeColor.white {
-                buttonOutlet.setText(str: "Project Life!", color: loadedColor!)
-            } else {
-                buttonOutlet.setText(str: "Project Life!", color : ThemeColor.black)
-            }
-        } catch {}
+        if loadedData != nil {
+            do {
+                try loadedColor =  NSKeyedUnarchiver.unarchivedObject(ofClasses : [NSColor.self], from: loadedData!) as? NSColor
+                if loadedColor != ThemeColor.white {
+                    buttonOutlet.setText(str: "Project Life!", color: loadedColor!)
+                } else {
+                    buttonOutlet.setText(str: "Project Life!", color : ThemeColor.black)
+                }
+            } catch {}
+        } else {
+    
+        }
+        
         //buttonOutlet.frame = .init(origin: buttonOutlet.frame.origin, size: .init(width: CGFloat(120), height: CGFloat(35)))
         buttonOutlet.wantsLayer = true
         buttonOutlet.layer?.cornerRadius = 20
@@ -67,18 +72,18 @@ class SignIn: NSViewController {
         if !buttonPushed {
             NSAnimationContext.current.duration = 1.5
             let diff = line1.frame.origin.y - line2.frame.origin.y
-            let line1EndOrigin = NSPoint.init(x: line1.frame.origin.x, y: line1.frame.origin.y + 250)
-            let line2EndOrigin = NSPoint.init(x: line2.frame.origin.x,y: line2.frame.origin.y + 250)
-            let buttonEndOrigin = NSPoint.init(x: sender.frame.origin.x, y: sender.frame.origin.y + 250)
+            let line1EndOrigin = NSPoint.init(x: line1.frame.origin.x, y: line1.frame.origin.y + 200)
+            let line2EndOrigin = NSPoint.init(x: line2.frame.origin.x,y: line2.frame.origin.y + 200)
+            let buttonEndOrigin = NSPoint.init(x: sender.frame.origin.x, y: sender.frame.origin.y + 200)
             line1.animator().setFrameOrigin(line1EndOrigin)
             line2.animator().setFrameOrigin(line2EndOrigin)
             sender.animator().setFrameOrigin(buttonEndOrigin)
             //line1.animator().translatesAutoresizingMaskIntoConstraints = true
-            let Constraint1 = NSLayoutConstraint.init(item: line1, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: -250)
+            let Constraint1 = NSLayoutConstraint.init(item: line1, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: -200)
             self.view.addConstraint(Constraint1)
             //self.view.addConstraint(Constraint2)
             line2.animator().setFrameOrigin(line2EndOrigin)
-            let cons = -250 + diff
+            let cons = -200 + diff
             let Constraint2 = NSLayoutConstraint.init(item: line2, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: cons)
             self.view.addConstraint(Constraint2)
             let ConstraintB = NSLayoutConstraint.init(item: sender, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: cons)
@@ -96,7 +101,7 @@ class SignIn: NSViewController {
             facebookButton.layer?.add(transition, forKey: nil)
             facebookButton.image = fbImage
             //facebookButton.isBordered = true
-            facebookButton.setFrameSize(.init(width: 433.33, height: 164.67))
+            facebookButton.setFrameSize(.init(width: 389.997, height: 148.203))
             facebookButton.imageScaling = .scaleProportionallyDown
             facebookButton.image?.alignmentRect = facebookButton.frame
             
@@ -104,7 +109,7 @@ class SignIn: NSViewController {
             googleButton.wantsLayer = true
             googleButton.layer?.add(transition, forKey: nil)
             googleButton.image = googleImage
-            googleButton.setFrameSize(.init(width: 503.96, height: 191.51))
+            googleButton.setFrameSize(.init(width: 453.564, height: 172.359))
             googleButton.imageScaling = .scaleProportionallyDown
             googleButton.image?.alignmentRect = googleButton.frame
             
