eb286e1bf71ef219a3a9a81c827f83760f753156
liumengda <liumengda@bytedance.com>
2019-08-16 16:22:58 +0800

    planner

--- ProjectLife/ViewControllers/ProjectPage/Sub/optionSub/planForDay.swift
+++ ProjectLife/ViewControllers/ProjectPage/Sub/optionSub/planForDay.swift
@@ -11,7 +11,7 @@ import Cocoa
 class planForDay: NSViewController {
 
     @IBOutlet weak var percentage: NSTextField!
-    @IBOutlet var comment: CustomTextView!
+    @IBOutlet var comment: PlaceholderTextView!
     @IBOutlet weak var dateMenu: NSMenu!
     var today : Date!
     var tom : Date!
@@ -20,16 +20,39 @@ class planForDay: NSViewController {
     var dayAfterTom : Date!
     var Selected : Date?
     
+    @IBOutlet weak var datePicker: NSPopUpButton!
     @IBOutlet weak var customDatePicker: NSDatePicker!
+    @IBOutlet weak var scroll: NSScrollView!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
-        let attributes : [NSAttributedString.Key : Any] = [NSAttributedString.Key(rawValue: NSAttributedString.Key.foregroundColor.rawValue) : NSColor.gray]
-        comment.placeholderAttributedString =  NSAttributedString(string: "optional comment on plan", attributes: attributes)
         
-    
+        self.view.wantsLayer = true
+        self.view.layer?.backgroundColor =  .init(gray: 0.9, alpha: 0.8)
+        NSLayoutConstraint.init(item: percentage!, attribute: .width, relatedBy: .greaterThanOrEqual, toItem: .none, attribute: .notAnAttribute, multiplier: 1, constant: 30).isActive = true
+        let initialPercentage = NSLayoutConstraint.init(item: percentage!, attribute: .width, relatedBy: .greaterThanOrEqual, toItem: .none, attribute: .notAnAttribute, multiplier: 1, constant: 130)
+        initialPercentage.priority = NSLayoutConstraint.Priority(rawValue: 250)
+        initialPercentage.isActive = true
+        let attributes : [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key(rawValue: NSAttributedString.Key.foregroundColor.rawValue) : NSColor.gray,
+            NSAttributedString.Key(rawValue : NSAttributedString.Key.font.rawValue) : NSFont.labelFont(ofSize: 14)
+        ]
+        
+        NSLayoutConstraint.init(item: datePicker, attribute: .width, relatedBy: .equal, toItem: .none, attribute: .notAnAttribute, multiplier: 1, constant: 200).isActive = true
+        comment.placeholderAttributedString = NSAttributedString.init(string: "optional comment", attributes: attributes)
+        comment.drawsBackground = true
+        comment.backgroundColor = NSColor.init(cgColor: CGColor.init(gray: 0.92, alpha: 0.5))!
+        //comment.placeholderAttributedString =  NSAttributedString(string: "optional   comment on plan", attributes: attributes)
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        customDatePicker.translatesAutoresizingMaskIntoConstraints = false
+        percentage.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.init(item: self.view, attribute: .height, relatedBy: .equal, toItem: .none, attribute: .notAnAttribute, multiplier: 1, constant: 100).isActive = true
+        
+        NSLayoutConstraint.init(item: scroll!, attribute: .height, relatedBy: .equal, toItem: .none, attribute: .notAnAttribute, multiplier: 1, constant: 74).isActive = true
+        NSLayoutConstraint.init(item: comment!, attribute: .height, relatedBy: .equal, toItem: .none, attribute: .notAnAttribute, multiplier: 1, constant: 74).isActive = true
         
         
         let today = Date.init()
@@ -43,35 +66,42 @@ class planForDay: NSViewController {
         self.tom = tommorrow
         let tommorrowHelper = dateUtils.init(date: tommorrow, relativeIndicator: 1)
         let tommorrowString = tommorrowHelper.forProjectSet()
-        dateMenu.addItem(withTitle: tommorrowString, action: #selector(self.handleSelectTom), keyEquivalent: "tab")
+        dateMenu.addItem(withTitle: tommorrowString, action: #selector(self.handleSelectTom), keyEquivalent: "command + 1")
         
         let dayAfterTom = todayHelper.getDay(addBy: 2)
         self.dayAfterTom = dayAfterTom
         let dayAfterTomHelper = dateUtils.init(date: dayAfterTom, relativeIndicator: 2)
         let dayAfterTomString = dayAfterTomHelper.forProjectSet()
-        dateMenu.addItem(withTitle: dayAfterTomString, action: #selector(self.handleSelectDayAfterTom), keyEquivalent: "tab")
+        dateMenu.addItem(withTitle: dayAfterTomString, action: #selector(self.handleSelectDayAfterTom), keyEquivalent: "command + 2")
         
         let yesterday = todayHelper.getDay(addBy: -1)
         self.yes = yesterday
         let yesterdayHelper = dateUtils.init(date: yesterday, relativeIndicator: -1)
         let yesterdayString = yesterdayHelper.forProjectSet()
-        dateMenu.addItem(withTitle: yesterdayString, action: #selector(self.handleSelectYes), keyEquivalent: "tab")
+        dateMenu.addItem(withTitle: yesterdayString, action: #selector(self.handleSelectYes), keyEquivalent: "command + 3")
         
         let dayBeforeYes = todayHelper.getDay(addBy: -2)
         self.dayBeforeYes = dayBeforeYes
         let dayBeforeYesHelper = dateUtils.init(date: dayBeforeYes, relativeIndicator: 2)
         let dayBeforeYesString = dayBeforeYesHelper.forProjectSet()
-        dateMenu.addItem(withTitle: dayBeforeYesString, action: #selector(self.handleSelectDayBeforeYes), keyEquivalent: "tab")
+        dateMenu.addItem(withTitle: dayBeforeYesString, action: #selector(self.handleSelectDayBeforeYes), keyEquivalent: "command + 4")
         
         dateMenu.addItem(withTitle: "custom", action: #selector(self.handleCustom)
-            , keyEquivalent: "tab")
+            , keyEquivalent: "command + 5")
         
-        customDatePicker.isEnabled = false
-        customDatePicker.isHidden = true
-        customDatePicker.dateValue = today
+       
+        
+        customDatePicker.isEnabled = true
+        customDatePicker.isHidden = false
+        //customDatePicker.dateValue = today
+        
+        for item in dateMenu.items {
+            item.isEnabled = true
+        }
     }
     
     @objc func handleSelectToday() {
+        print("hello world")
         self.Selected = self.today
         customDatePicker.isEnabled = false
         customDatePicker.isHidden = true
@@ -109,8 +139,15 @@ class planForDay: NSViewController {
     @IBAction func sendCustomValue(_ sender: NSDatePicker) {
         self.Selected = sender.dateValue
     }
+    @IBAction func Confirm(_ sender: Any) {
+        print("fuckfuckfuck")
+    }
+    
 }
 
-class CustomTextView: NSTextView {
-    var placeholderAttributedString: NSAttributedString?
+
+   
+class PlaceholderTextView: NSTextView {
+    @objc var placeholderAttributedString: NSAttributedString?
 }
+   
--- ProjectLife/ViewControllers/ProjectPage/Sub/options.swift
+++ ProjectLife/ViewControllers/ProjectPage/Sub/options.swift
@@ -13,7 +13,7 @@ class options: NSViewController {
     @IBOutlet weak var plan: HoverButton!
     @IBOutlet weak var work: HoverButton!
     @IBOutlet weak var archive: HoverButton!
-    
+    var droppedDownSetter : NSViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +32,20 @@ class options: NSViewController {
         plan.setText(str: "Plan For Day", color: ThemeColor.red)
         work.setText(str: "Record Work For Day", color: ThemeColor.green)
         archive.setText(str: "Archive", color: ThemeColor.blue)
+    }
+    
+    @IBAction func plan(_ sender: Any) {
+        let p = planForDay.init(nibName: "planForDay", bundle: nil)
+        p.view.translatesAutoresizingMaskIntoConstraints = false 
+        (self.parent!.parent as! projectStack).Stack.insertArrangedSubview(p.view, at: 2)
+        (self.parent!.parent as! projectStack).addChild(p)
+        (self.parent!.parent as! projectStack).Stack.setCustomSpacing(0, after: p.view)
+    }
+    
+    @IBAction func work(_ sender: Any) {
+    }
+    
+    @IBAction func archive(_ sender: Any) {
     }
     
 }
